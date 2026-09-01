1b8db18fc4547127eeae7c1853a2462e7e48aa5e
Zhendi Cao <metrixgo9@gmail.com>
2026-08-31 22:32:03 -0700

    made crouch, tuned some other controls

--- Assets/Script/Universal/PlayerController.cs
+++ Assets/Script/Universal/PlayerController.cs
@@ -15,19 +15,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform playerHold;
 
     private float walkSpeed = 3f;
-    private float sprintSpeed = 5f;
+    private float sprintSpeed = 6f;
     private float crouchSpeed = 1.5f;
-    private float standHeight = 0.75f;
-    private float crouchHeight = -0.6f;
-    private float jumpStrength = 3f;
+    private float standHeight = 2f;
+    private float crouchHeight = 0.6f;
+    private float standCamHeight = 1.75f;
+    private float crouchCamHeight = 0.4f;
+    private float jumpStrength = 6f;
     private float gravity = -12f;
-    private float transitionLength = 0.2f;
+    private float groundGravity = -1f;
+    private float transitionLength = 0.3f;
     private float reachRange = 1.5f;
     private float normalFieldOfView = 60f;
     private float sprintFieldOfView = 65f;
     private float sensitivity = 5f;
 
     private bool isCrouched = false;
+    private float camHeight = 0.75f;
     private float rotationX = 0f;
     private float velocityY = -1f;
 
@@ -63,7 +67,7 @@ public class PlayerController : MonoBehaviour
 
     private void UpdateState()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0.01f || Input.GetAxisRaw("Vertical") > 0.01f)
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.01f || Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.01f)
         {
             if (isCrouched) state = PlayerState.CrouchWalk;
             else if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) state = PlayerState.Sprint;
@@ -89,22 +93,28 @@ public class PlayerController : MonoBehaviour
         offsets[0] = Mathf.Sin(camBobbingT * 2 * Mathf.PI / 3f) * 0.01f;
         offsets[1] = Mathf.Sin(camBobbingT * 2 * Mathf.PI / 0.8f) * 0.05f;
         offsets[2] = Mathf.Sin(camBobbingT * 2 * Mathf.PI / 0.5f) * 0.08f;
-        offsets[3] = Mathf.Sin(camBobbingT * 2 * Mathf.PI / 2f) * 0.007f;
-        offsets[4] = Mathf.Sin(camBobbingT * 2 * Mathf.PI / 0.7f) * 0.03f;
+        offsets[3] = Mathf.Sin(camBobbingT * 2 * Mathf.PI / 3f) * 0.005f;
+        offsets[4] = Mathf.Sin(camBobbingT * 2 * Mathf.PI / 0.8f) * 0.025f;
 
         float weightSum = 0f;
         float offsetSum = 0f;
+        int curState = (int)state;
+        if (!controller.isGrounded) curState = 0;
+
         for (int i = 0; i < weights.Length; i++)
         {
-            if (i != (int)state) weights[i] = Mathf.MoveTowards(weights[i], 0f, Time.deltaTime / transitionLength);
+            if (i != curState) weights[i] = Mathf.MoveTowards(weights[i], 0f, Time.deltaTime / transitionLength);
             else weights[i] = Mathf.MoveTowards(weights[i], 1f, Time.deltaTime / transitionLength);
             weightSum += weights[i];
             offsetSum += weights[i] * offsets[i];
         }
-        if (weightSum > 0f) playerCam.transform.localPosition = new Vector3(0f, standHeight + offsetSum / weightSum, 0f);
 
-        if (state == PlayerState.Sprint) playerCam.fieldOfView = Mathf.MoveTowards(playerCam.fieldOfView, sprintFieldOfView, Mathf.Abs(sprintFieldOfView - normalFieldOfView) * Time.deltaTime / transitionLength);
-        else playerCam.fieldOfView = Mathf.MoveTowards(playerCam.fieldOfView, normalFieldOfView, Mathf.Abs(sprintFieldOfView - normalFieldOfView) * Time.deltaTime / transitionLength);
+        float goalCamHeight = isCrouched ? crouchCamHeight : standCamHeight;
+        camHeight = Mathf.MoveTowards(camHeight, goalCamHeight, Mathf.Abs(standCamHeight - crouchCamHeight) * Time.deltaTime / transitionLength);
+        if (weightSum > 0f) playerCam.transform.localPosition = new Vector3(0f, camHeight + offsetSum / weightSum, 0f);
+
+        float goalView = state == PlayerState.Sprint ? sprintFieldOfView : normalFieldOfView;
+        playerCam.fieldOfView = Mathf.MoveTowards(playerCam.fieldOfView, goalView, Mathf.Abs(sprintFieldOfView - normalFieldOfView) * Time.deltaTime / transitionLength);
     }
 
     private void MovePlayer()
@@ -113,10 +123,24 @@ public class PlayerController : MonoBehaviour
         if (state == PlayerState.Sprint) speed = sprintSpeed;
         if (isCrouched) speed = crouchSpeed;
 
-        if (controller.isGrounded) velocityY = -1f;
-        else velocityY += gravity * Time.deltaTime;
+        if (controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.C)) isCrouched = !isCrouched;
+            if (Input.GetKeyDown(KeyCode.Space) && !isCrouched) velocityY = jumpStrength;
+            else velocityY = groundGravity;
+        }
+        else
+        {
+            velocityY += gravity * Time.deltaTime;
+        }
+
+        float goalHeight = isCrouched ? crouchHeight : standHeight;
+        controller.height = Mathf.MoveTowards(controller.height, goalHeight, Mathf.Abs(standHeight - crouchHeight) * Time.deltaTime / transitionLength);
+        controller.center = Vector3.up * controller.height * 0.5f;
 
         Vector3 move = (transform.right * Input.GetAxisRaw("Horizontal") + transform.forward * Input.GetAxisRaw("Vertical")).normalized * speed;
-        controller.Move((move + Vector3.up * velocityY) * Time.deltaTime);
+        
+        CollisionFlags flags = controller.Move((move + Vector3.up * velocityY) * Time.deltaTime);
+        if ((flags & CollisionFlags.Above) != 0 && velocityY > 0f) velocityY = groundGravity;
     }
 }
